18de7ae47bfa219dae3af137c30816fd5986a784
YoshuaGES <auhsoywatchandlearn@gmail.com>
2024-11-06 18:20:24 +0700

    Update index.js

--- 01-writing-comments/index.js
+++ 01-writing-comments/index.js
@@ -1,5 +1,7 @@
 // username: yosh_gg_PNva
 
-// Goal tahun ini:
-// 1. Belajar JavaScript.
-// 2. Menjadi Front-End atau Back-End Developer.
+/*
+Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer.
+*/
